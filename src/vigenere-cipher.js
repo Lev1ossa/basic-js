7b76b6f701dfd6b332ade651d00f68ad4a32045e
Lev1ossa <ikehhui@gmail.com>
2023-04-17 03:22:27 +0600

    feat: complete 10/21 subtasks (Mon, Apr 17, 2023 03:22:27 AM)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,114 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+const AlphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect) {
+    if(isDirect == false) {
+      this.isDirect = false;
+    } else {
+      this.isDirect = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      let messageArr = message.toUpperCase().split('').map(item => {
+        if (AlphabetString.includes(item)) {
+          return AlphabetString.search(`[${item}]`) + 1 > 26 ? AlphabetString.search(`[${item}]`) - 26 + 1: AlphabetString.search(`[${item}]`) + 1;
+        } else {
+          return 0;
+        }
+      });
+      let keyArr = key.toUpperCase().split('').map(item => {
+        if (AlphabetString.includes(item)) {
+          return AlphabetString.search(`[${item}]`) + 1 > 26 ? AlphabetString.search(`[${item}]`) - 26 + 1: AlphabetString.search(`[${item}]`) + 1;
+        } else {
+          return 0;
+        }
+      });
+      
+      let keyArrLength = keyArr.length;
+      let zeroCounter = 0;
+
+      for (let i = 0; i < messageArr.length; i++) {
+        if(messageArr[i] > 0) {
+          if(i - zeroCounter >= keyArrLength) {
+            if((i - zeroCounter) / keyArrLength >= 2){
+              let letterIdx = messageArr[i] + keyArr[(i - zeroCounter) % keyArrLength] - 2;
+              letterIdx = letterIdx + 1 > 26 ? letterIdx - 26 : letterIdx;
+              messageArr[i] = AlphabetString[letterIdx];
+            } else {
+              let letterIdx = messageArr[i] + keyArr[i - zeroCounter - keyArrLength] - 2;
+              letterIdx = letterIdx + 1 > 26 ? letterIdx - 26 : letterIdx;
+              messageArr[i] = AlphabetString[letterIdx];
+            }
+          } else {
+            let letterIdx = messageArr[i] + keyArr[i - zeroCounter] - 2;
+            letterIdx = letterIdx + 1 > 26 ? letterIdx - 26 : letterIdx;
+            messageArr[i] = AlphabetString[letterIdx];
+          }
+        } else {
+          messageArr[i] = message[i];
+          zeroCounter += 1;
+        }
+      }
+      
+      return this.isDirect ? messageArr.join('') : messageArr.reverse().join('');
+    }
+  }
+
+  decrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      let messageArr = message.toUpperCase().split('').map(item => {
+        if (AlphabetString.includes(item)) {
+          return AlphabetString.search(`[${item}]`) + 1 > 26 ? AlphabetString.search(`[${item}]`) - 26 + 1: AlphabetString.search(`[${item}]`) + 1;
+        } else {
+          return 0;
+        }
+      });
+      let keyArr = key.toUpperCase().split('').map(item => {
+        if (AlphabetString.includes(item)) {
+          return AlphabetString.search(`[${item}]`) + 1 > 26 ? AlphabetString.search(`[${item}]`) - 26 + 1: AlphabetString.search(`[${item}]`) + 1;
+        } else {
+          return 0;
+        }
+      });
+      
+      let keyArrLength = keyArr.length;
+      let zeroCounter = 0;
+
+      for (let i = 0; i < messageArr.length; i++) {
+        if(messageArr[i] > 0) {
+          if(i - zeroCounter >= keyArrLength) {
+            if((i - zeroCounter) / keyArrLength >= 2){
+              let letterIdx = messageArr[i] - keyArr[(i - zeroCounter) % keyArrLength];
+              letterIdx = letterIdx < 0 ? 26 + letterIdx : letterIdx;
+              messageArr[i] = AlphabetString[letterIdx];
+            } else {
+              let letterIdx = messageArr[i] - keyArr[i - zeroCounter - keyArrLength];
+              letterIdx = letterIdx < 0 ? 26 + letterIdx : letterIdx;
+              messageArr[i] = AlphabetString[letterIdx];
+            }
+          } else {
+            let letterIdx = messageArr[i] - keyArr[i - zeroCounter];
+            letterIdx = letterIdx < 0 ? 26 + letterIdx : letterIdx;
+            messageArr[i] = AlphabetString[letterIdx];
+          }
+        } else {
+          messageArr[i] = message[i];
+          zeroCounter += 1;
+        }
+      }
+      
+      return this.isDirect ? messageArr.join('') : messageArr.reverse().join('');
+    }
   }
 }
 
